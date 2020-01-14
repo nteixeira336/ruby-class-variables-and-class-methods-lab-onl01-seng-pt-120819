caf9dc41f87729ed7da1d342de3a20d821126fb7
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-01-14 16:12:48 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,45 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  
+  @@count=0 
+  
+  def initialize (name, artist, genre)
+    @name= name
+    @artist= artist
+    @@artists<< artist 
+    @genre= genre 
+    @@genres<< genre 
+    @@count+=1 
+    
+  end
+  
+  def self.count 
+    @@count 
+  end 
+  
+  @@genres= [ ]
+  
+  def self.genres
+    @@genres.uniq 
+  end 
+  
+  @@artists= [ ]
+  
+  def self.artists 
+    artists= { } 
+  
+   def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+  
+end 
+  
+  
